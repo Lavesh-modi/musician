9503d034deda4b7a4268ff8ace205385668d501b
Lavesh-modi <laveshmodiag@gmail.com>
2024-03-21 17:06:38 +0530

    landing Page

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <HeroSection/>
+    <FeaturedCourses></FeaturedCourses>
+    </>
+  //   <div className="h-[100vh] flex justify-center">
+  //  <div className="flex items-center text-2xl">Start</div>
+   
+  //  </div>
+  );
+}
