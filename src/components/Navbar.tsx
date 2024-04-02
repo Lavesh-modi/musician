248cf49e16c464f775ed939651d52a9d4140b9c5
Lavesh-modi <laveshmodiag@gmail.com>
2024-04-02 16:51:24 +0530

    Signup|page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,9 +9,8 @@ function Navbar({ className }: { className?: string }) {
   return (
     <div className={cn("fixed top-12 inset-x-0 max-w-2xl mx-auto z-50")}>
       <Menu setActive={setActive}>
-        <Link href={"#"}>
+        <Link href={"/"}>
           <MenuItem setActive={setActive} active={active} item="Home">
-            test
           </MenuItem>
         </Link>
         <MenuItem setActive={setActive} active={active} item="Our Course">
